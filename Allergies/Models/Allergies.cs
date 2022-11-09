93d9b7ce2a666a90c7793694b25cce7fcdd5de5b
Quin-Riley-A <quin.asselin1@gmail.com>
2022-11-08 22:33:43 -0500

    construct architecture for TDD
    Co-authored-by : Remy Flores <remyfranciscoflores@gmail.com>

---
+++ Allergies/Models/Allergies.cs
@@ -0,0 +1,11 @@
+namespace Allergies
+{
+  public class Allergens
+  {
+    public string[] MakesMeSick = {"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"};
+    public string SickoZone(int score)
+    {
+      
+    }
+  }
+}
