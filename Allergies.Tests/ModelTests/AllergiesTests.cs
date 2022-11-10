8e74c117c74c4f72260e2d01092b286ab809e4e3
Quin-Riley-A <quin.asselin1@gmail.com>
2022-11-09 23:45:58 -0500

    receive good fail on third test
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Remy Flores <remyfranciscoflores@gmail.com>

--- Allergies.Tests/ModelTests/AllergiesTests.cs
+++ Allergies.Tests/ModelTests/AllergiesTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Allergies;
+using System;
+using System.Linq;
 
 namespace Allergies.Tests
 {
@@ -19,6 +21,17 @@ namespace Allergies.Tests
       Allergens testAllergens = new Allergens();
 
       Assert.AreEqual("eggs, ", testAllergens.SickoZone(1));
+    }
+    [TestMethod]
+    public void SickoZone_ReturnsAllAllergens_String()
+    {
+      Allergens testAllergens = new Allergens();
+      string[] reverseArray = Enumerable.Reverse(testAllergens.MakesMeSick).ToArray();
+      Console.WriteLine(reverseArray);
+      Console.WriteLine(testAllergens.MakesMeSick);
+      string bigAllergensString = String.Join(' ', reverseArray);
+      bigAllergensString = bigAllergensString.Replace(" ", ", ");
+      Assert.AreEqual(bigAllergensString, testAllergens.SickoZone(255));
     }
   }
 }
